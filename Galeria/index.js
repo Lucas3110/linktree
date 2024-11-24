0c8615fddab6464aaeefc89287cdff3fc6f0bd75
Lucas3110 <lucas.arg.rodriguez@gmail.com>
2024-11-24 11:58:12 -0300

    Proyecto final

---
+++ Galeria/index.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const slides = document.querySelectorAll('.slide');
+  const bullets = document.querySelectorAll('.bullet');
+  const nextBtn = document.getElementById('next');
+  const prevBtn = document.getElementById('prev');
+  let currentSlide = 0;
+  let slideInterval;
+
+  function showSlide(index, direction) {
+    slides[currentSlide].classList.remove('current');
+    bullets[currentSlide].classList.remove('current');
+
+    slides[currentSlide].classList.add(direction === 'next' ? 'exit-left' : 'exit-right');
+    slides[index].classList.add(direction === 'next' ? 'enter-right' : 'enter-left');
+
+    setTimeout(() => {
+      slides[currentSlide].classList.remove('exit-left', 'exit-right');
+      slides[index].classList.remove('enter-left', 'enter-right');
+
+      currentSlide = index;
+
+      slides[currentSlide].classList.add('current');
+      bullets[currentSlide].classList.add('current');
+    }, 1000);
+  }
+
+  function nextSlide() {
+    let nextIndex = (currentSlide + 1) % slides.length;
+    showSlide(nextIndex, 'next');
+  }
+
+  function prevSlide() {
+    let prevIndex = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(prevIndex, 'prev');
+  }
+
+  bullets.forEach((bullet, index) => {
+    bullet.addEventListener('click', () => {
+      if (index !== currentSlide) {
+        let direction = index > currentSlide ? 'next' : 'prev';
+        showSlide(index, direction);
+        resetInterval();
+      }
+    });
+  });
+
+  nextBtn.addEventListener('click', () => {
+    nextSlide();
+    resetInterval();
+  });
+
+  prevBtn.addEventListener('click', () => {
+    prevSlide();
+    resetInterval();
+  });
+
+  function startSlideInterval() {
+    slideInterval = setInterval(nextSlide, 5000);
+  }
+
+  function resetInterval() {
+    clearInterval(slideInterval);
+    startSlideInterval();
+  }
+
+
+  // Iniciar el cambio automático de diapositivas
+  startSlideInterval();
+});
